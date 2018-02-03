a6ec3eacd64c0fb4786c956182ddce3e4d64c675
Alice Rimassa <alice@eponae.fr>
2018-02-03 21:24:50 +0100

    Updated dotfiles

--- install.sh
+++ install.sh
@@ -6,6 +6,15 @@ for file in ${HOME}/Documents/dev/dotfiles/symlinks/*; do
   [ -r "${file}" ] && [ -f "${file}" ] && rm -f ${HOME}/.`basename ${file}` && ln -s ${file} ${HOME}/.`basename ${file}`
 done
 
-echo 'Host *
+MAX_OS_SSH_CONFIG=""
+if [ `uname` == 'Darwin' ]; then
+  MAX_OS_SSH_CONFIG="
+    UseKeyChain no"
+fi
+
+echo "Host *
+    PasswordAuthentication no
+    ChallengeResponseAuthentication no
+    HashKnownHosts yes${MAX_OS_SSH_CONFIG}
     ServerAliveInterval 300
-    ServerAliveCountMax 2' > ${HOME}/.ssh/config
+    ServerAliveCountMax 2" >> ${HOME}/.ssh/config
